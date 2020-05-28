0c0a93b59a6acb15a9e6e0d2f81856fce6064ca0
sjschweber <schweber97@gmail.com>
2020-05-28 16:50:52 -0400

    Updates homepage

--- src/components/scroll-list.js
+++ src/components/scroll-list.js
@@ -4,9 +4,37 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Img from '../images/dark-knight.jpg'
 import Jaw from '../images/jaw.png'
+import { Link, StaticQuery, useStaticQuery, graphql} from "gatsby"
+
 import './scroll.css'
 
+
+
 export default function ScrollList(){
+
+  const data = useStaticQuery(graphql`
+    query{
+       allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              author
+              genre
+              slug
+              image
+          }
+        }
+      }
+    }
+  }
+}
+`)
+
+const newPosts = data.allMarkdownRemark.edges
+.sort((a, b) => b.frontmatter.date - a.frontmatter.date).slice(0, 2);
 
   return(
 
@@ -16,10 +44,7 @@ export default function ScrollList(){
           <ChevronLeftIcon/>
         </IconButton>
       </div>
-      <ul class="images">
-        <li><img src={Img}/></li>
-        <li><img src={Jaw}/></li>
-      </ul>
+        <img src= {newPosts[0].frontmatter.image}/>
       <div>
         <IconButton>
           <ChevronRightIcon/>
